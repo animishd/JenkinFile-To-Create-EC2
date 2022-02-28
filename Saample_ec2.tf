466ec5a5e57f8abb907149899b3a15808bb5be0a
animishd <96632144+animishd@users.noreply.github.com>
2022-02-28 23:50:14 +0530

    Update Saample_ec2.tf

--- Saample_ec2.tf
+++ Saample_ec2.tf
@@ -1,10 +1,9 @@
 provider "aws" {
   region = "ap-south-1"
-  access_key = "AKIA4CGVURHJATSYZNZI"
-  secret_key = "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"
+  access_key = "AKIA4CGVURHJPPQ74S3T"
+  secret_key = "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"
 }
-resource "aws_instance" "ec2" {
+resource "aws_instance" "my_ec2" {
   ami = "ami-0c6615d1e95c98aca"
   instance_type = "t2.micro"
-    
 }
